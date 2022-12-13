0abc1ec83e085afeee49970c7025b31218ceca79
Stone Comstock <comstockstone@yahoo.com>
2022-12-12 20:19:55 -0800

    Set up redux for registering and authenticating users

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_URL = '/api/users/';
+
+// Register User
+const register = async (userData) => {
+  const response = await axios.post(API_URL, userData);
+
+  // Check for response data, set localStorage 'user' to response data
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+const authService = { register };
---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,67 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import authService from './authService';
+import { toast } from 'react-toastify';
+
+// Get the user from localStorage if it exists.
+const user = JSON.parse(localStorage.getItem('user'));
+
+// Set initialState
+const initialState = {
+  user: user ? user : null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+};
+
+// Register the user
+export const register = createAsyncThunk(
+  'auth/register',
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      const message = toast.error(
+        `Cannot register user. Error: ${error.message}`
+      );
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+const authSlice = createSlice({
+  name: '/auth/register',
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = false;
+      state.message = '';
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      // Register
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isError = true;
+        state.isSuccess = false;
+        state.isLoading = false;
+        state.message = action.payload;
+        state.user = null;
+      });
+  },
+});
+
+export const {} = authSlice.actions;
+
+export default authSlice.reducer;
