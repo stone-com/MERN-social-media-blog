8bdbc78e810d97906cd81c206838e4ba30b193bc
Stone Comstock <comstockstone@yahoo.com>
2022-12-20 17:39:02 -0800

    Bug fix with deployment

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,13 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+}
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../frontend/src/index.html'))
+);
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/posts', require('./routes/postRoutes'));
